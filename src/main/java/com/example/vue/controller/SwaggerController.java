77fd88d0342090e72d71dba933abfb52bf7123d4
J-Hoon0227 <invierno902@gmail.com>
2023-12-13 13:23:30 +0900

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   .mvn/wrapper/maven-wrapper.jar
    	new file:   .mvn/wrapper/maven-wrapper.properties
    	new file:   mvnw
    	new file:   mvnw.cmd
    	new file:   pom.xml
    	new file:   src/main/java/com/example/vue/VueApplication.java
    	new file:   src/main/java/com/example/vue/config/SwaggerConfig.java
    	new file:   src/main/java/com/example/vue/controller/SwaggerController.java
    	new file:   src/main/resources/application.properties
    	new file:   src/main/resources/static/css/app.2cf79ad6.css
    	new file:   src/main/resources/static/favicon.ico
    	new file:   src/main/resources/static/index.html
    	new file:   src/main/resources/static/js/app.9407343d.js
    	new file:   src/main/resources/static/js/app.9407343d.js.map
    	new file:   src/main/resources/static/js/chunk-vendors.8a6123a4.js
    	new file:   src/main/resources/static/js/chunk-vendors.8a6123a4.js.map
    	new file:   src/test/java/com/example/vue/VueApplicationTests.java
    	new file:   vue/webpack/.gitignore
    	new file:   vue/webpack/README.md
    	new file:   vue/webpack/babel.config.js
    	new file:   vue/webpack/jsconfig.json
    	new file:   vue/webpack/package-lock.json
    	new file:   vue/webpack/package.json
    	new file:   vue/webpack/public/favicon.ico
    	new file:   vue/webpack/public/index.html
    	new file:   vue/webpack/src/App.vue
    	new file:   vue/webpack/src/assets/logo.png
    	new file:   vue/webpack/src/components/HelloWorld.vue
    	new file:   vue/webpack/src/main.js
    	new file:   vue/webpack/vue.config.js
    
     Changes not staged for commit:
    	modified:   src/main/java/com/example/vue/controller/SwaggerController.java
    	modified:   src/main/resources/static/index.html
    	deleted:    src/main/resources/static/js/app.9407343d.js
    	deleted:    src/main/resources/static/js/app.9407343d.js.map
    	modified:   vue/webpack/src/App.vue
    
     Untracked files:
    	src/main/resources/static/js/app.35e27c1d.js
    	src/main/resources/static/js/app.35e27c1d.js.map

---
+++ src/main/java/com/example/vue/controller/SwaggerController.java
@@ -0,0 +1,38 @@
+package com.example.vue.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@Tag(name = "예제 API", description = "Swagger 테스트용 API")
+@RestController
+@RequestMapping("/")
+public class SwaggerController {
+    @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
+    @Parameter(name = "str", description = "2번 반복할 문자열") //파라미터를 지정해주어야 함
+    @GetMapping("/returnStr")
+    public String returnStr(@RequestParam String str) {
+        return str + "\n" + str;
+    }
+ 
+    @GetMapping("/example")
+    public String example() {
+        return "예시 API";
+    }
+
+    @GetMapping("/context")
+    public String context() {
+        return "예시 context API";
+    }
+ 
+    @Hidden
+    @GetMapping("/ignore")
+    public String ignore() {
+        return "무시되는 API";
+    }
+}
---
+++ vue/webpack/vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  outputDir:"../../src/main/resources/static",
+  // devServer:{
+  //   prot: 8081,
+  //   proxy:  'http://localhost:8080',
+  //   disableHostCheck: true
+  // },
+});
